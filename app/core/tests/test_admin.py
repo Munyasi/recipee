c9e76b0a7c5306467588087ee472e503359c5416
munyasi <psmunyasi@gmail.com>
2019-09-17 14:53:48 +0300

    test push

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='abc@localhost.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@localhost.com'
+        )
